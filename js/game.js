467ffe2c449c4a073b989201cbf779e6ec40bbc3
jvsantoz <joao.vitor.santossilva.cba@gmail.com>
2022-11-29 00:23:33 -0400

    feat(header)player name and timer

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
 
 const characters = [
     'CARTA_BOY',
@@ -24,8 +26,10 @@ const checkEndGame = () => {
     const disabledCards = document.querySelectorAll('.disabled-card');
 
     if (disabledCards.length === 16) {
+        
         setTimeout(() =>{
-            alert('Parabéns, você conseguiu!!')
+            clearInterval(this.loop);
+            alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML} segundos`)
 
         }, 600);
     }
@@ -114,4 +118,20 @@ const loadGame = () => {
 
 }
 
-loadGame();
+const startTimer = () => {
+    
+    this.loop = setInterval(() => {
+
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;    
+
+    }, 1000);
+}
+
+window.onload = () => {
+    
+    spanPlayer.innerHTML = localStorage.getItem('player');
+    
+    startTimer();
+    loadGame();
+}
